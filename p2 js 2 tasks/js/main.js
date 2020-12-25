7db1ad906decb2724c5d77c4ad98f1bb578b6d73
hager-mamdouh <hager.mamdouh112@gmail.com>
2020-12-25 14:58:20 +0200

    push

---
+++ p2 js 2 tasks/js/main.js
@@ -0,0 +1,14 @@
+let num=Number(prompt("enter number"));
+if (num%2===0){
+    console.log (`Number is Even`);
+}else{
+    console.log(`Number is odd`);
+}
+
+let age= Number(prompt("enter your age"));
+if(age>18){
+    console.log(`you logged in succssfully`)
+}else{
+    console.log(`you can't login because your age below 18`)
+}
+
